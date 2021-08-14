4bcf5cd081a453771e323d090d177fbc5c4c2027
Surya Jasper <suryajasper2004@gmail.com>
2021-08-14 11:46:32 -0700

    Added a deadband and scaled the motor output using a transfer function.

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -4,10 +4,11 @@
 
 #include "Robot.h"
 
+using namespace std;
 
 void Robot::RobotInit() {
 
-  lMotorFront->SetInverted(true);
+  lMotorFront->SetInverted(false);
   lMotorBack->Follow(*lMotorFront, false);
   rMotorFront->SetInverted(false);
   rMotorBack->Follow(*rMotorFront, false);
@@ -29,12 +30,29 @@ void Robot::TeleopInit() {}
 void Robot::TeleopPeriodic() {
 // L_X = 0, L_Y = 1, R_X = 4, R_Y = 5
 // GetRawAxis(1)
+double dz = 0.08;
+double x = y_j->GetY();
+double abs_x = abs(x);
 
-lMotorFront->Set(y_j->GetY());
-rMotorFront->Set(y_j->GetY());
+if (abs_x > dz) {
+// 1/(1-dz) 1.0869 * 0.08 + (1 - (1.0869)) = 0
+// 1/(1-(-0.08)) 0.9259 * -0.08 + (1 - 0.9259)
 
+  double y = (1/(1-dz)) * abs_x + (1 - (1/(1-dz)));
+  //y = y * y;
+  //double y = std::copysign(y, x);
+
+  if (x < 0) {
+    y = -y;
+  }
+  lMotorFront->Set(y);
+  rMotorFront->Set(-y);
+
+} else {
+  lMotorFront->Set(0);
+  rMotorFront->Set(0);
 }
-
+}
 void Robot::DisabledInit() {}
 void Robot::DisabledPeriodic() {}
 
