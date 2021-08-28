54041f4899564375f3df7364b59b90faf384ab6d
Aarush Kachhawa <aarushkachhawa@gmail.com>
2021-08-27 18:55:57 -0700

    Minor changes

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -8,15 +8,17 @@ using namespace std;
 
 void Robot::RobotInit() {
 
-  lMotorFront->SetInverted(false);
-  lMotorBack->Follow(*lMotorFront, false);
-  rMotorFront->SetInverted(false);
-  rMotorBack->Follow(*rMotorFront, false);
-
   lMotorFront->RestoreFactoryDefaults();
   lMotorBack->RestoreFactoryDefaults();
   rMotorFront->RestoreFactoryDefaults();
   rMotorBack->RestoreFactoryDefaults();
+
+  lMotorFront->SetInverted(true);
+  lMotorBack->Follow(*lMotorFront, false);
+  rMotorFront->SetInverted(false);
+  rMotorBack->Follow(*rMotorFront, false);
+
+  
 
 }
 
@@ -29,24 +31,25 @@ void Robot::AutonomousPeriodic() {}
 void Robot::TeleopInit() {}
 void Robot::TeleopPeriodic() {
 // L_X = 0, L_Y = 1, R_X = 4, R_Y = 5
-// GetRawAxis(1)
-double dz = 0.08;
-double x = y_j->GetY();
-double abs_x = abs(x);
 
-if (abs_x > dz) {
-// 1/(1-dz) 1.0869 * 0.08 + (1 - (1.0869)) = 0
-// 1/(1-(-0.08)) 0.9259 * -0.08 + (1 - 0.9259)
 
-  double y = (1/(1-dz)) * abs_x + (1 - (1/(1-dz)));
+double dz = 0.08; // Deadzone
+double x = y_j->GetY(); // Raw Y value
+
+if (abs(x) > dz) {
+
+  double y = (1/(1-dz)) * abs(x) + (1 - (1/(1-dz)));
+  y = copysign(y, x);
+
   //y = y * y;
-  //double y = std::copysign(y, x);
 
-  if (x < 0) {
-    y = -y;
-  }
+  //if (x < 0) {
+    //y = -y;
+  //}
   lMotorFront->Set(y);
   rMotorFront->Set(-y);
+
+  // TODO: Invert Motors
 
 } else {
   lMotorFront->Set(0);
